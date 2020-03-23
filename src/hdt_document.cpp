4aa82b6b3bc93399873cd58be6088fae27f606de
Javier <jfernand@wu.ac.at>
2020-03-23 23:02:03 +0100

    add predef wikidata

--- src/hdt_document.cpp
+++ src/hdt_document.cpp
@@ -267,7 +267,7 @@ void HDTDocument::configureHops(int setnumHops,vector<unsigned int> filterPredic
 	filterPrefixStr = setfilterPrefixStr;
 
 	// get the ID of literals if needed
-	if (setfilterPrefixStr=="" || setfilterPrefixStr!="predef-dbpedia2016-04"){
+	if (setfilterPrefixStr=="" || setfilterPrefixStr!="predef-dbpedia2016-04" || setfilterPrefixStr!="predef-wikidata2020-03-all"){
 		IteratorUCharString * itObjects = hdt->getDictionary()->getObjects();
 		bool foundNonString=false;
 		literalEndID = hdt->getDictionary()->getNshared();
@@ -289,6 +289,9 @@ void HDTDocument::configureHops(int setnumHops,vector<unsigned int> filterPredic
 			preffixIniSUBJECT=50097212;
 			preffixEndSUBJECT=52750736;
 			literalEndID=147777579;
+		}
+		else if (setfilterPrefixStr=="predef-wikidata2020-03-all"){
+			literalEndID=1924886681;
 		}
 		else{
 			IteratorUInt *itIDSol = hdt->getDictionary()->getIDSuggestions(setfilterPrefixStr.c_str(),SUBJECT);
