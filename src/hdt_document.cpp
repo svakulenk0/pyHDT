94878eae724e52eeb4b3674153bbea44ef747d5c
Javier <jfernand@wu.ac.at>
2020-04-02 21:30:12 +0200

    add predef limits for wikidata 2018

--- src/hdt_document.cpp
+++ src/hdt_document.cpp
@@ -267,7 +267,7 @@ void HDTDocument::configureHops(int setnumHops,vector<unsigned int> filterPredic
 	filterPrefixStr = setfilterPrefixStr;
 
 	// get the ID of literals if needed
-	if (setfilterPrefixStr=="" || (setfilterPrefixStr!="predef-dbpedia2016-04"&& setfilterPrefixStr!="predef-wikidata2020-03-all")){
+	if (setfilterPrefixStr=="" || (setfilterPrefixStr!="predef-dbpedia2016-04"&& setfilterPrefixStr!="predef-wikidata2020-03-all" && setfilterPrefixStr!="predef-wikidata2018-09-all")){
 		IteratorUCharString * itObjects = hdt->getDictionary()->getObjects();
 		bool foundNonString=false;
 		literalEndID = hdt->getDictionary()->getNshared();
@@ -292,6 +292,9 @@ void HDTDocument::configureHops(int setnumHops,vector<unsigned int> filterPredic
 		}
 		else if (setfilterPrefixStr=="predef-wikidata2020-03-all"){
 			literalEndID=1924886681;
+		}
+		else if (setfilterPrefixStr=="predef-wikidata2018-09-all"){
+			literalEndID=1145542077;
 		}
 		else{
 			IteratorUInt *itIDSol = hdt->getDictionary()->getIDSuggestions(setfilterPrefixStr.c_str(),SUBJECT);
